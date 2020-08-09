7826eb829b20ecb98f36668655a3e92d4e669348
김효욱 <hyowoog@gimhyoug-ui-iMac.local>
2020-08-09 12:01:01 +0900

    Updates

---
+++ precache-manifest.9e3c247db0dfd4a9287ed48b4a31493c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caa6737095bba80feded69ef14c92e9",
+    "url": "/github.io/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e0d5735fd08828ed9ff2",
+    "url": "/github.io/movie_app_2019/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "fde87d2ad6cabe52f608",
+    "url": "/github.io/movie_app_2019/static/js/2.c3d73534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/movie_app_2019/static/js/2.c3d73534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d5735fd08828ed9ff2",
+    "url": "/github.io/movie_app_2019/static/js/main.c5eb8dd6.chunk.js"
+  },
+  {
+    "revision": "cd1254ab6b815c01e635",
+    "url": "/github.io/movie_app_2019/static/js/runtime-main.87d440fc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hyowoog/movie_app_2019/precache-manifest.7c25754b25b3122a82132d5c66cf723e.js"
+  "/github.io/movie_app_2019/precache-manifest.9e3c247db0dfd4a9287ed48b4a31493c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hyowoog/movie_app_2019/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/movie_app_2019/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
